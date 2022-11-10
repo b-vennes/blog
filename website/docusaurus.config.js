124555c559d0ec77997bc7f877ddc8be124ed6ba
Branden Vennes <b.n.vennes@gmail.com>
2022-11-10 10:06:03 -0600

    Tweaked publish

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Branden Vennes',
   tagline: 'Functional Programming, Scala, and AWS',
-  url: 'https://www.brandenvennes.com/',
+  url: 'https://www.brandenvennes.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -18,6 +18,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'b-vennes', // Usually your GitHub org/user name.
   projectName: 'blog', // Usually your repo name.
+
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
